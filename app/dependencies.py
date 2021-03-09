ce1ade6394b3e6b299bd62a16f66a907c6b3def9
Araz B. Makoo <araz100@gmail.com>
2021-03-09 10:05:59 +0100

    changed name to osiris-ingress

--- app/dependencies.py
+++ app/dependencies.py
@@ -18,7 +18,7 @@ class Configuration:
 
     def __init__(self, name: str):
         self.config = configparser.ConfigParser()
-        self.config.read(['conf.ini', '/etc/osiris/conf.ini'])
+        self.config.read(['conf.ini', '/etc/osiris/conf.ini', '/etc/osiris-ingress/conf.ini'])
 
         logging.config.fileConfig(fname=self.config['Logging']['configuration_file'], disable_existing_loggers=False)
 
